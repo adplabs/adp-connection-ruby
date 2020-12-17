f80c73ecdee00fd7e3835253244eab1ccc64e993
Sergey Mazur <sergey@decisely.com>
2020-12-17 10:57:23 -0700

    [JUNO-7478] Add more logging
    https://decisely.atlassian.net/browse/JUNO-7478

--- lib/adp/marketplace/api.rb
+++ lib/adp/marketplace/api.rb
@@ -3,14 +3,11 @@
 module Adp
   module Marketplace
     module API
-      def event_information(event_url)
+      def event_information(event_url, logger_class = ::Adp::Connection.marketplace_api_logger)
+        api_logger = logger_class.new
         options = { headers: headers }
         api_logger.log_request(uri: event_url, options: options)
         self.class.get(event_url, options).tap { |response| api_logger.log_response(response: response) }
-      end
-
-      def api_logger
-        @adp_logger ||= ::Adp::Connection.marketplace_api_logger.new
       end
 
       private
